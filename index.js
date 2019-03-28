253e45c0b1cc50caa3e4518b10ec7634a174a969
Ashish Garg <mragarg@gmail.com>
2019-03-28 10:31:54 -0400

    Adds server test and nodemon

--- index.js
+++ index.js
@@ -16,5 +16,19 @@ const port = 3000; // This is the port number we'll listen on
 // The Request is what came from the browser.
 // The Response is "half-baked" and ready for you to send it back.
 const server = http.createServer((req, res) => {
-    
+    // We could console.log some things about the request.
+    console.log(req.url);
+
+    // The status code "200" means "Everything is awesome."
+    res.statusCode = 200;
+
+    // Declare the nature of the contents.
+    res.setHeader('Content-Type', 'text/plain')
+
+    // Ship it!
+    res.end('Hello Test\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}`)
 });
